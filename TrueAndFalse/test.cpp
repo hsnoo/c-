85006f05549a45aa61ce97afb430acdfd815a190
hyunsoo Noh <hyunsoo061956@gmail.com>
2021-12-22 11:47:05 +0900

    first commit

---
+++ TrueAndFalse/test.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main(void)
+//{
+//	int num1 = true;
+//	int num2 = false;
+//	int num3 = true + false;
+//
+//	cout << num1 << endl;
+//	cout << num2 << endl;
+//	cout << num3 << endl;
+//}
